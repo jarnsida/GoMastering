a700495824e18b223ef2c8739c0753abbbf1f9b8
Eugene Sviat <oceaos@gmail.com>
2021-08-09 02:27:07 +0300

    JSON rename field

--- JSON/main.go
+++ JSON/main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	type CrewwMember struct {
-		ID                int32
-		Name              string
-		SecurityClearance int32
-		AccessCodes       []string
+		ID                int      `json:"id,omitempty"`
+		Name              string   `json:"name"`
+		SecurityClearance int      `json:"clearanceLevel"`
+		AccessCodes       []string `json:"accesscodes"`
 	}
 
 	cm := CrewwMember{1, "Jacob", 10, []string{"ADA", "LAL"}}
